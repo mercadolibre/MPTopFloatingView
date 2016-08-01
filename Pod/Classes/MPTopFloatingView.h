53370209a7fae5652771af0e4be16e780f65fab8
samuelsainz <samuel.sainz@mercadolibre.com>
2016-08-01 10:08:06 -0300

    Can get the current status of the floating view. Can set the text inside the view after creating it. Auto dismiss feature

--- Pod/Classes/MPTopFloatingView.h
+++ Pod/Classes/MPTopFloatingView.h
@@ -11,9 +11,30 @@
  Use this enum to indicate the status of the view
  */
 typedef NS_ENUM(NSInteger, MPTopFloatingViewStatus) {
-	MPTopFloatingViewStatusAppear = 0,
-	MPTopFloatingViewStatusDisappear = 1,
+    MPTopFloatingViewStatusAppear = 0,
+    MPTopFloatingViewStatusDisappear = 1,
 };
+
+/**
+ *  Specify the dismiss cause for the Floating View
+ */
+typedef NS_ENUM(NSInteger, MPTopFloatingViewDismissCause) {
+    /**
+     *  Floating view is dismissed because user tap on the view
+     */
+    MPTopFloatingViewDismissCauseTap,
+    /**
+     *  Floating view is dismissed because auto dismiss was enabled.
+     */
+    MPTopFloatingViewDismissCauseAuto
+};
+
+/**
+ *  Dismiss block used as a callback when the floating view is dismissed
+ *
+ *  @param cause of the dismiss
+ */
+typedef void (^MPTopFloatingViewDismissBlock)(MPTopFloatingViewDismissCause cause);
 
 @interface MPTopFloatingView : UIView
 
@@ -21,7 +42,7 @@ typedef NS_ENUM(NSInteger, MPTopFloatingViewStatus) {
 /// @name Initialization
 ///---------------------
 
-/** 
+/**
  Use this method to create an instance with the default configuration
  @param newsActionHandler The block to be executed when the view is tapped
  */
@@ -36,8 +57,21 @@ typedef NS_ENUM(NSInteger, MPTopFloatingViewStatus) {
  */
 - (nonnull instancetype)initTopFloatingViewWithText:(nonnull NSString *)text color:(nonnull UIColor *)color icon:(nonnull UIImage *)icon onTapBlock:(nullable void (^)())newsActionHandler;
 
+
 /**
- This is the designated initializer
+ *  Use this method to create a floating view with custom text, color and the time for the view to be shown.
+ *
+ *  @param text              The text inside the view
+ *  @param color             Color to fill the background view
+ *  @param timeToDismiss     After this time the view will be dismissed (in seconds)
+ *  @param newsActionHandler The block to be executed when the view is tapped
+ *
+ */
+- (nonnull instancetype)initTopFloatingViewWithText:(nonnull NSString *)text color:(nonnull UIColor *)color timeToDismiss:(NSTimeInterval)timeToDismiss dismissBlock:(MPTopFloatingViewDismissBlock)dismissBlock onTapBlock:(nullable void (^)())newsActionHandler;
+
+
+/**
+ Use this method to create a floating view with a full customization. The view will be not auto dismissed.
  @param text The text inside the view
  @param textFont With this param you can change the font type of the text
  @param textColor Customize the color of the text
@@ -48,6 +82,20 @@ typedef NS_ENUM(NSInteger, MPTopFloatingViewStatus) {
  */
 - (nonnull instancetype)initTopFloatingViewWithText:(nonnull NSString *)text textFont:(nullable UIFont *)font textColor:(nullable UIColor *)textColor color:(nonnull UIColor *)color icon:(nonnull UIImage *)icon finalPosition:(float)finalPosition duration:(float)duration onTapBlock:(nullable void (^)())newsActionHandler;
 
+/**
+ This is the designated initializer
+ @param text The text inside the view
+ @param textFont With this param you can change the font type of the text
+ @param textColor Customize the color of the text
+ @param color Color to fill the background view
+ @param icon Icon to add inside the view
+ @param finalPosition Use this param to determine the final position of the view when appear on the screen
+ @param timeToDismiss After this time the view will be dismissed
+ @param dismissBlock The block to be executed when the view is dismissed
+ @param newsActionHandler The block to be executed when the view is tapped
+ */
+- (nonnull instancetype)initTopFloatingViewWithText:(nonnull NSString *)text textFont:(nullable UIFont *)font textColor:(nullable UIColor *)textColor color:(nonnull UIColor *)color icon:(nonnull UIImage *)icon finalPosition:(float)finalPosition duration:(float)duration timeToDismiss:(NSTimeInterval)timeToDismiss dismissBlock:(MPTopFloatingViewDismissBlock)dismissBlock onTapBlock:(nullable void (^)())newsActionHandler;
+
 ///---------------------
 /// @name Animations
 ///---------------------
@@ -57,5 +105,15 @@ typedef NS_ENUM(NSInteger, MPTopFloatingViewStatus) {
  @param status The status is a representation of the desire animation
  */
 - (void)startAnimation:(MPTopFloatingViewStatus)status;
+
+/**
+ *  Text inside the view. Set this property to change the message after creating the view.
+ */
+@property (strong, nonatomic) NSString *text;
+
+/**
+ *  The current status of the floating view (read-only)
+ */
+@property (readonly) MPTopFloatingViewStatus status;
 
 @end
--- Pod/Classes/MPTopFloatingView.m
+++ Pod/Classes/MPTopFloatingView.m
@@ -30,6 +30,12 @@ typedef void (^NewsActionHandler)();
 @property (nonatomic) float initialPositionY;
 @property (nonatomic) float initialPositionX;
 @property (nonatomic) float finalPosition;
+
+// Auto dismiss
+@property (strong, nonatomic) NSTimer *timer;
+@property (nonatomic) NSTimeInterval timeToDismiss;
+@property (nonatomic, copy) MPTopFloatingViewDismissBlock dismissBlock;
+
 @end
 
 @implementation MPTopFloatingView
@@ -37,164 +43,213 @@ typedef void (^NewsActionHandler)();
 #pragma mark initializers
 - (nonnull instancetype)initTopFloatingViewWithOnTapBlock:(nullable void (^)())newsActionHandler
 {
-	// Setup the default configuration
-	self = [self initTopFloatingViewWithText:@"New Activities" color:[UIColor colorWithRed:0 green:0.62 blue:0.89 alpha:1] icon:[UIImage imageNamed:@"up-arrow"] onTapBlock:newsActionHandler];
-	return self;
+    // Setup the default configuration
+    self = [self initTopFloatingViewWithText:@"New Activities" color:[UIColor colorWithRed:0 green:0.62 blue:0.89 alpha:1] icon:[UIImage imageNamed:@"up-arrow"] onTapBlock:newsActionHandler];
+    return self;
 }
 
 - (nonnull instancetype)initTopFloatingViewWithText:(nonnull NSString *)text color:(nonnull UIColor *)color icon:(nonnull UIImage *)icon onTapBlock:(nullable void (^)())newsActionHandler
 {
-	
-	// Setup a default final position and duration if this method is used
-	self = [self initTopFloatingViewWithText:text textFont:nil textColor:nil color:color icon:icon finalPosition:30 duration:0.5 onTapBlock:newsActionHandler];
-	
-	return self;
+    // Setup a default final position and duration if this method is used
+    self = [self initTopFloatingViewWithText:text textFont:nil textColor:nil color:color icon:icon finalPosition:30 duration:0.5 onTapBlock:newsActionHandler];
+    
+    return self;
 }
 
 - (nonnull instancetype)initTopFloatingViewWithText:(nonnull NSString *)text textFont:(nullable UIFont *)font textColor:(nullable UIColor *)textColor color:(nonnull UIColor *)color icon:(nonnull UIImage *)icon finalPosition:(float)finalPosition duration:(float)duration onTapBlock:(nullable void (^)())newsActionHandler
 {
-	if (self = [super init]) {
-		
-		// Verify the param required
-		NSAssert(color, @"Color can not be nil.");
-		NSAssert(icon, @"Icon can not be nil.");
-		NSAssert(text, @"Text can not be nil.");
-		NSAssert(finalPosition, @"finalPosition can not be nil.");
-		NSAssert(duration, @"duration can not be nil.");
-		NSAssert(text.length, @"Text can not be empty.");
-	
-		self = [[[NSBundle mainBundle] loadNibNamed:@"MPTopFloatingView" owner:self options:nil] lastObject];
-	
-		// Setup the style
-		[self setupStyleWithText:text textFont:font textColor:textColor finalPosition:finalPosition duration:duration backgroundColor:color icon:icon completionBlock:newsActionHandler];
-		[self handleTapAction];
-	}
-	
-	return self;
+    self = [self initTopFloatingViewWithText:text textFont:font textColor:textColor color:color icon:icon finalPosition:finalPosition duration:duration timeToDismiss:0 dismissBlock:nil onTapBlock:newsActionHandler];
+    
+    return self;
+}
+
+- (nonnull instancetype)initTopFloatingViewWithText:(nonnull NSString *)text color:(nonnull UIColor *)color timeToDismiss:(NSTimeInterval)timeToDismiss dismissBlock:(MPTopFloatingViewDismissBlock)dismissBlock onTapBlock:(nullable void (^)())newsActionHandler
+{
+    self = [self initTopFloatingViewWithText:text textFont:nil textColor:nil color:color icon:[UIImage imageNamed:@"up-arrow"] finalPosition:30 duration:0.5 timeToDismiss:timeToDismiss dismissBlock:dismissBlock onTapBlock:newsActionHandler];
+    
+    return self;
+}
+
+- (nonnull instancetype)initTopFloatingViewWithText:(nonnull NSString *)text textFont:(nullable UIFont *)font textColor:(nullable UIColor *)textColor color:(nonnull UIColor *)color icon:(nonnull UIImage *)icon finalPosition:(float)finalPosition duration:(float)duration timeToDismiss:(NSTimeInterval)timeToDismiss dismissBlock:(MPTopFloatingViewDismissBlock)dismissBlock onTapBlock:(nullable void (^)())newsActionHandler
+{
+    if (self = [super init]) {
+        
+        // Verify the param required
+        NSAssert(color, @"Color can not be nil.");
+        NSAssert(icon, @"Icon can not be nil.");
+        NSAssert(text, @"Text can not be nil.");
+        NSAssert(finalPosition, @"finalPosition can not be nil.");
+        NSAssert(duration, @"duration can not be nil.");
+        NSAssert(text.length, @"Text can not be empty.");
+        
+        self = [[[NSBundle mainBundle] loadNibNamed:@"MPTopFloatingView" owner:self options:nil] lastObject];
+        
+        // Setup the style
+        [self setupStyleWithText:text textFont:font textColor:textColor finalPosition:finalPosition duration:duration backgroundColor:color icon:icon timeToDismiss:timeToDismiss dismissBlock:dismissBlock completionBlock:newsActionHandler];
+        [self handleTapAction];
+    }
+    
+    return self;
 }
 
 #pragma mark setup
-- (void)setupStyleWithText:(NSString *)text textFont:(UIFont *)font textColor:(UIColor *)textColor finalPosition:(float)finalPosition duration:(float)duration backgroundColor:(UIColor *)color icon:(UIImage *)icon completionBlock:(void (^)())completionBlock
+- (void)setupStyleWithText:(NSString *)text textFont:(UIFont *)font textColor:(UIColor *)textColor finalPosition:(float)finalPosition duration:(float)duration backgroundColor:(UIColor *)color icon:(UIImage *)icon timeToDismiss:(NSTimeInterval)timeToDismiss dismissBlock:(MPTopFloatingViewDismissBlock)dismissBlock completionBlock:(void (^)())completionBlock
 {
-	self.completionBlock = completionBlock;
-	self.layer.cornerRadius = CGRectGetHeight(self.bounds)/2;
-	self.currentStatus = MPTopFloatingViewStatusDisappear;
-	self.backgroundColor = color;
-	self.textLabel.text = text;
-	self.iconImageView.image = icon;
-	self.finalPosition = finalPosition;
-	self.duration = duration;
-	
-	if (font) {
-		[self.textLabel setFont:font];
-	}
-	
-	if (textColor) {
-		[self.textLabel setTextColor:textColor];
-	}
+    self.completionBlock = completionBlock;
+    self.layer.cornerRadius = CGRectGetHeight(self.bounds)/2;
+    self.currentStatus = MPTopFloatingViewStatusDisappear;
+    self.backgroundColor = color;
+    self.textLabel.text = text;
+    self.iconImageView.image = icon;
+    self.finalPosition = finalPosition;
+    self.duration = duration;
+    self.timeToDismiss = timeToDismiss;
+    self.dismissBlock = dismissBlock;
+    
+    if (font) {
+        [self.textLabel setFont:font];
+    }
+    
+    if (textColor) {
+        [self.textLabel setTextColor:textColor];
+    }
 }
 
 // This method make all the changes in the view to match the final result of the animation
 - (void)setupFinalState:(MPTopFloatingViewStatus)status
 {
-	if (status == MPTopFloatingViewStatusAppear) {
-		self.currentStatus = MPTopFloatingViewStatusAppear;
-		self.center = CGPointMake(self.initialPositionX, self.finalPosition);
-		return;
-	}
-	
-	if (status == MPTopFloatingViewStatusDisappear) {
-		self.currentStatus = MPTopFloatingViewStatusDisappear;
-		self.center = CGPointMake(self.initialPositionX, self.initialPositionY);
-		return;
-	}
+    if (status == MPTopFloatingViewStatusAppear) {
+        if (self.timeToDismiss > 0) {
+            self.timer = [NSTimer scheduledTimerWithTimeInterval:self.timeToDismiss
+                                                          target:self
+                                                        selector:@selector(autoDismiss)
+                                                        userInfo:nil
+                                                         repeats:NO];
+        }
+        self.currentStatus = MPTopFloatingViewStatusAppear;
+        self.center = CGPointMake(self.initialPositionX, self.finalPosition);
+        return;
+    }
+    
+    if (status == MPTopFloatingViewStatusDisappear) {
+        self.currentStatus = MPTopFloatingViewStatusDisappear;
+        self.center = CGPointMake(self.initialPositionX, self.initialPositionY);
+        return;
+    }
+}
+
+- (void)setText:(NSString *)text
+{
+    self.textLabel.text = text;
+}
+
+- (NSString *)text
+{
+    return self.textLabel.text;
+}
+
+- (MPTopFloatingViewStatus)status
+{
+    return self.currentStatus;
 }
 
 #pragma mark animations
 - (void)startAnimation:(MPTopFloatingViewStatus)status
 {
-	self.animLayer = self.layer;
-	
-	// Save the initial position of the view
-	if (!self.initialPositionY && !self.initialPositionX) {
-		self.initialPositionX = CGRectGetMidX(self.frame);
-		self.initialPositionY = CGRectGetMinY(self.frame);
-	}
-	
-	if (status == MPTopFloatingViewStatusAppear) {
-		// perform the animation depending the iOS version
-		if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"9.0")) {
-			[self showSpringAnimation];
-		} else {
-			[self showAnimation];
-		}
-		return;
-	}
-	
-	if (status == MPTopFloatingViewStatusDisappear) {
-		[self hideAnimation];
-		return;
-	}
+    self.animLayer = self.layer;
+    
+    // Save the initial position of the view
+    if (!self.initialPositionY && !self.initialPositionX) {
+        self.initialPositionX = CGRectGetMidX(self.frame);
+        self.initialPositionY = CGRectGetMinY(self.frame);
+    }
+    
+    if (status == MPTopFloatingViewStatusAppear) {
+        // perform the animation depending the iOS version
+        if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"9.0")) {
+            [self showSpringAnimation];
+        } else {
+            [self showAnimation];
+        }
+        return;
+    }
+    
+    if (status == MPTopFloatingViewStatusDisappear) {
+        [self hideAnimation];
+        return;
+    }
 }
 
 // This animation is used for old system versions
 - (void)showAnimation
 {
-	// Prevent show the view if is not hidden
-	if (self.currentStatus == MPTopFloatingViewStatusDisappear) {
-		CABasicAnimation *showAnimation = [CABasicAnimation animationWithKeyPath:@"position.y"];
-		showAnimation.duration = 0.2;
-		showAnimation.fromValue = [NSNumber numberWithFloat:self.initialPositionY];
-		showAnimation.toValue = [NSNumber numberWithFloat:self.finalPosition];
-		[self setupFinalState:MPTopFloatingViewStatusAppear];
-		[self.layer addAnimation:showAnimation forKey:nil];
-	}
+    // Prevent show the view if is not hidden
+    if (self.currentStatus == MPTopFloatingViewStatusDisappear) {
+        CABasicAnimation *showAnimation = [CABasicAnimation animationWithKeyPath:@"position.y"];
+        showAnimation.duration = 0.2;
+        showAnimation.fromValue = [NSNumber numberWithFloat:self.initialPositionY];
+        showAnimation.toValue = [NSNumber numberWithFloat:self.finalPosition];
+        [self setupFinalState:MPTopFloatingViewStatusAppear];
+        [self.layer addAnimation:showAnimation forKey:nil];
+    }
 }
 
 - (void)showSpringAnimation
 {
-	// Prevent show the view if is not hidden
-	if (self.currentStatus == MPTopFloatingViewStatusDisappear) {
-		CASpringAnimation *spring = [CASpringAnimation animationWithKeyPath:@"position.y"];
-		spring.damping = 5;
-		spring.fromValue = [NSNumber numberWithFloat:self.initialPositionY];
-		spring.toValue = [NSNumber numberWithFloat:self.finalPosition];
-		spring.duration = self.duration;
-		[self setupFinalState:MPTopFloatingViewStatusAppear];
-		[self.layer addAnimation:spring forKey:nil];
-	}
+    // Prevent show the view if is not hidden
+    if (self.currentStatus == MPTopFloatingViewStatusDisappear) {
+        CASpringAnimation *spring = [CASpringAnimation animationWithKeyPath:@"position.y"];
+        spring.damping = 5;
+        spring.fromValue = [NSNumber numberWithFloat:self.initialPositionY];
+        spring.toValue = [NSNumber numberWithFloat:self.finalPosition];
+        spring.duration = self.duration;
+        [self setupFinalState:MPTopFloatingViewStatusAppear];
+        [self.layer addAnimation:spring forKey:nil];
+    }
 }
 
 - (void)hideAnimation
 {
-	// Prevent hide the view if is not already presented
-	if (self.currentStatus == MPTopFloatingViewStatusAppear) {
-		CABasicAnimation *hideAnimation = [CABasicAnimation animationWithKeyPath:@"position.y"];
-		hideAnimation.duration = 0.2;
-		hideAnimation.fromValue = [NSNumber numberWithFloat:self.finalPosition];
-		hideAnimation.toValue = [NSNumber numberWithFloat:self.initialPositionY];
-		hideAnimation.removedOnCompletion = YES;
-		[self setupFinalState:MPTopFloatingViewStatusDisappear];
-		[self.layer addAnimation:hideAnimation forKey:nil];
-	}
+    // Prevent hide the view if is not already presented
+    if (self.currentStatus == MPTopFloatingViewStatusAppear) {
+        CABasicAnimation *hideAnimation = [CABasicAnimation animationWithKeyPath:@"position.y"];
+        hideAnimation.duration = 0.2;
+        hideAnimation.fromValue = [NSNumber numberWithFloat:self.finalPosition];
+        hideAnimation.toValue = [NSNumber numberWithFloat:self.initialPositionY];
+        hideAnimation.removedOnCompletion = YES;
+        [self setupFinalState:MPTopFloatingViewStatusDisappear];
+        [self.layer addAnimation:hideAnimation forKey:nil];
+    }
+}
+
+- (void)autoDismiss
+{
+    [self.timer invalidate];
+    [self startAnimation:MPTopFloatingViewStatusDisappear];
+    if (self.dismissBlock) {
+        self.dismissBlock(MPTopFloatingViewDismissCauseAuto);
+    }
 }
 
 - (void)handleTapAction
 {
-	UITapGestureRecognizer *gesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(viewTapped:)];
-	self.userInteractionEnabled = YES;
-	[self addGestureRecognizer:gesture];
+    UITapGestureRecognizer *gesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(viewTapped:)];
+    self.userInteractionEnabled = YES;
+    [self addGestureRecognizer:gesture];
 }
 
 // Tap action
 - (void)viewTapped:(UIGestureRecognizer *) sender
 {
-	[self startAnimation:MPTopFloatingViewStatusDisappear];
-	
-	if (self.completionBlock) {
-		self.completionBlock();
-	}
+    [self startAnimation:MPTopFloatingViewStatusDisappear];
+    
+    if (self.completionBlock) {
+        self.completionBlock();
+    }
+    
+    if (self.dismissBlock) {
+        self.dismissBlock(MPTopFloatingViewDismissCauseTap);
+    }
 }
 
 @end
-
